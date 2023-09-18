9a9c044c71573a9cabf014552ffaefec34282921
wonju <jwwon00@naver.com>
2023-09-18 16:33:12 +0900

    drill 2 제출 2019182025 원정우

--- character_grass.py
+++ character_grass.py
@@ -4,14 +4,8 @@ import math
 open_canvas()
 
 # fill here
-# 사진 불러오기
 grass = load_image('grass.png')
 character = load_image('character.png')
-
-#grass.draw_now(400, 30)
-#character.draw_now(400, 90)
-
-#delay(5)
 
 count =0
 
@@ -38,19 +32,19 @@ while(True):
 
  x2 = 400
  y2 = 300
- a = 360
+ a = 90
  r = 150
  
  #게임 루프
  while (count!=0):
-  clear_canvas_now()
-  grass.draw_now(400, 30)
-  character.draw_now(x2, y2)
 
   x2=r*math.cos(math.radians(a))+400
   y2=r*math.sin(math.radians(a))+300
   a=a-5
-  delay(0.02)
-  if(a==0):
+  delay(0.1)
+  clear_canvas_now()
+  grass.draw_now(400, 30)
+  character.draw_now(x2, y2)
+  if(a==-270):
     count = 0
 close_canvas()
