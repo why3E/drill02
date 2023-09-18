533e0723ab09ac4b65d3f4e0be244e11bd5383ec
wonju <jwwon00@naver.com>
2023-09-18 15:19:42 +0900

    drill 02 제출 2019182025

--- character_grass.py
+++ character_grass.py
@@ -1,7 +1,34 @@
 from pico2d import *
+import math
 
 open_canvas()
 
-# fill here
+# 사진 불러오기
+grass = load_image('grass.png')
+character = load_image('character.png')
 
+#grass.draw_now(400, 30)
+#character.draw_now(400, 90)
+
+#delay(5)
+
+x = 0
+y=90
+
+#게임 루프
+while (True):
+ #게임 렌더링
+ clear_canvas_now()
+ grass.draw_now(400, 30)
+ character.draw_now(x, y)
+ #게임 로직(상호작용을 계산하는 것)
+ if(x!=770 and y==90):
+  x = x + 2 
+ elif(x==770 and y!=540):
+  y= y+2
+ elif(x!=0 and y==540):
+  x=x-2
+ elif(x==0 and y!=90):
+  y=y-2
+ delay(0.01)
 close_canvas()
